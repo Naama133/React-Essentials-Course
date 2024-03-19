2042ac3df5d04527613f857c5ce15bf5b629901d
Naama133 <naamab@wix.com>
2024-03-19 14:22:11 +0200

    Adding props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import ComponentsImg from './assets/components.png';
+
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -20,12 +22,48 @@ function Header() {
   );
 }
 
+function CoreConcept(props)
+{
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={ComponentsImg} >
+            </CoreConcept>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={ComponentsImg} >
+            </CoreConcept>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={ComponentsImg} >
+            </CoreConcept>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={ComponentsImg} >
+            </CoreConcept>
+          </ul>
+        </section>
       </main>
     </div>
   );
