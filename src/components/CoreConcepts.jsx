9470e0a3f0aec7ed54253c6fd9d3d91b296d8ed1
Naama133 <naamab@wix.com>
2024-03-21 10:12:04 +0200

    Splitting components by feature & state

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,14 @@
+import {CORE_CONCEPTS} from '../data.js';
+import CoreConcept from './CoreConcept/CoreConcept.jsx'
+
+export default function CoreConcepts()
+{
+    return (
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          {CORE_CONCEPTS.map((concept) => <CoreConcept key={concept.title} {...concept} />)}
+        </ul>
+      </section>
+    );
+}
